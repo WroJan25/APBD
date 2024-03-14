cc3282fd8b3e28d940659cbe2745362a6b4d62f4
s28044 <s28044@pjwstk.edu.pl>
2024-03-14 22:12:12 +0100

    kolejnaFunkcjonalnosc

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,8 +5,10 @@ static double GetAVG(double[] tab)
 {
     int[] liczby = { 10, 20, 30, 40, 50 };
     double srednia = ObliczSrednia(liczby);
+    int maksimum = ZnajdzMaksimum(liczby);
+
     Console.WriteLine("Średnia wynosi: " + srednia);
-    
+    Console.WriteLine("Maksimum wynosi: " + maksimum);
     double sum = 0;
     foreach (var VARIABLE in tab)
     {
@@ -46,4 +48,22 @@ static double ObliczSrednia(int[] tablica)
     }
 
     return (double)suma / tablica.Length;
+}
+static int ZnajdzMaksimum(int[] tablica)
+{
+    if (tablica.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie może być pusta.");
+    }
+
+    int maksimum = tablica[0];
+    foreach (int liczba in tablica)
+    {
+        if (liczba > maksimum)
+        {
+            maksimum = liczba;
+        }
+    }
+
+    return maksimum;
 }
